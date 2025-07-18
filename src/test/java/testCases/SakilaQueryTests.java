158f2b744a16ffb103e16493f8c2814f39eae5ba
dwijavyas <dwija21@gmail.com>
2025-07-18 13:11:39 -0400

    initial commit

---
+++ src/test/java/testCases/SakilaQueryTests.java
@@ -0,0 +1,13 @@
+package testCases;
+
+public class SakilaQueryTests {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+
+	}
+
+}
---
+++ src/test/java/testCases/e2edbQueryTests.java
@@ -0,0 +1,10 @@
+package testCases;
+
+public class e2edbQueryTests {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
